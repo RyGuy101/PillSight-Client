26727aca3e2a389608ff37448664345115f71df8
Ryan Nemiroff <ryguyn@gmail.com>
2021-06-03 03:23:14 -0700

    Updates

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Webcam from "react-webcam";
+import { Button } from "reactstrap";
 
 import "./App.css";
+
+const videoConstraints = {
+  facingMode: { exact: "environment" },
+};
 
 const App = () => {
   return (
     <div className="App">
-      <Webcam audio={false} imageSmoothing={false} />
+      <Webcam
+        audio={false}
+        imageSmoothing={false}
+        videoConstraints={videoConstraints}
+      />
+      <Button color="danger" id="scan-btn">
+        Scan
+      </Button>
     </div>
   );
 };
