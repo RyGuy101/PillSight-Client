cafe3bfe252d7b5cd3aba2152529bb818be9d3fe
Ryan Nemiroff <ryguyn@gmail.com>
2021-06-03 03:07:39 -0700

    Add webcam and deploy to GitHub Pages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Webcam from "react-webcam";
 
 import "./App.css";
 
 const App = () => {
-  return <div className="App">Hello, World!</div>;
+  return (
+    <div className="App">
+      <Webcam audio={false} imageSmoothing={false} />
+    </div>
+  );
 };
 
 export default App;
